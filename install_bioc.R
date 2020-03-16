2cb318cd7d3116af5424fdfffa795006a1728cca
catavallejos <cata.vallejos.m@gmail.com>
2020-03-16 23:37:59 +0000

    - Adds checks for uninstalled bioc packages

--- install_bioc.R
+++ install_bioc.R
@@ -1,1 +1,13 @@
 BiocManager::install("BiocStyle")
+
+pkgs1 <- c(
+  "BiocStyle"
+)
+
+BiocManager::install(pkgs1, update = FALSE, Ncpus = 4)
+
+# List installed packages
+installed_pkgs <- as.vector(installed.packages()[,1])
+
+# Check if anything is not installed 
+print(setdiff(pkgs1,installed_pkgs))
